f01fed53d2eab949f12a89a7711a0b167ad5f7ec
Congbo Song <congbo.song@ncas.ac.uk>
2024-06-19 13:38:56 +0100

    publish package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def install_with_pip(package):
 required_packages = [
     "pandas", "numpy", "scipy", "joblib", "flaml", "matplotlib", "seaborn",
     "ruptures", "scikit-learn", "statsmodels", "cdsapi", "pyreadr", "wget",
-    "xarray","cvxpy"
+    "xarray"
 ]
 
 for package in required_packages:
@@ -16,7 +16,7 @@ for package in required_packages:
 
 setup(
     name="normet",
-    version="0.1.0",
+    version="0.1.2",
     description="Normet for automated air quality intervention studies",
     long_description=open("README.rst", "r", encoding="utf-8").read(),
     long_description_content_type="text/x-rst",
