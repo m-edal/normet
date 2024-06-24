81d9da0cb6646f24089940eaf40e4dc288d9faad
Congbo Song <congbo.song@ncas.ac.uk>
2024-06-24 18:03:05 +0100

    Optimize autodew

--- normet/pdp.py
+++ normet/pdp.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.inspection import PartialDependenceDisplay
 from sklearn.base import clone
 
-def pdp_all(automl, df, feature_names=None,variables=None, training_only=True,n_cores = -1):
+def pdp_all(model, df, feature_names=None,variables=None, training_only=True,n_cores = -1):
     """
     Computes partial dependence plots for all specified features.
 
@@ -27,18 +27,18 @@ def pdp_all(automl, df, feature_names=None,variables=None, training_only=True,n_
         df = df[df["set"] == "training"]
     X_train, y_train = df[feature_names], df['value']
 
-    results = Parallel(n_jobs=n_cores)(delayed(pdp_worker)(automl,X_train,var) for var in variables)
+    results = Parallel(n_jobs=n_cores)(delayed(pdp_worker)(model,X_train,var) for var in variables)
     df_predict = pd.concat(results)
     df_predict.reset_index(drop=True, inplace=True)
     return df_predict
 
 
-def pdp_worker(automl, X_train, variable,training_only=True):
+def pdp_worker(model, X_train, variable,training_only=True):
     """
     Worker function for computing partial dependence plots for a single feature.
 
     Parameters:
-        automl: AutoML model object.
+        model: AutoML model object.
         X_train (DataFrame): Input DataFrame containing the training data.
         variable (str): Name of the feature to compute partial dependence plot for.
         training_only (bool, optional): If True, computes partial dependence plot only for the training set. Default is True.
@@ -47,7 +47,7 @@ def pdp_worker(automl, X_train, variable,training_only=True):
         DataFrame: DataFrame containing the computed partial dependence plot for the specified feature.
     """
     # Filter only to training set
-    results = partial_dependence(estimator=automl, X=X_train,
+    results = partial_dependence(estimator=model, X=X_train,
                                  features=variable,kind='individual')
 
     # Alter names and add variable
@@ -60,12 +60,12 @@ def pdp_worker(automl, X_train, variable,training_only=True):
     return df_predict
 
 
-def pdp_plot(automl,df,feature_names,variables=None,kind='average',n_cores=-1,training_only=True,figsize=(8,8),hspace=0.5):
+def pdp_plot(model,df,feature_names,variables=None,kind='average',n_cores=-1,training_only=True,figsize=(8,8),hspace=0.5):
     """
     Plots partial dependence plots for specified features.
 
     Parameters:
-        automl: AutoML model object.
+        model: AutoML model object.
         df (DataFrame): Input DataFrame containing the dataset.
         feature_names (list): List of feature names to plot partial dependence plots for.
         variables (list, optional): List of variables to plot partial dependence plots for. If None, defaults to feature_names.
@@ -85,17 +85,17 @@ def pdp_plot(automl,df,feature_names,variables=None,kind='average',n_cores=-1,tr
         df = df[df["set"] == "training"]
     X_train, y_train = df[feature_names], df['value']
     fig, ax = plt.subplots(figsize=figsize)
-    result = PartialDependenceDisplay.from_estimator(automl, X_train, variables,kind=kind,n_jobs=n_cores,ax=ax)
+    result = PartialDependenceDisplay.from_estimator(model, X_train, variables,kind=kind,n_jobs=n_cores,ax=ax)
     plt.subplots_adjust(hspace=hspace)
     return result
 
 
-def pdp_interaction(automl,df,variables,kind='average',training_only=True,ncols=3,figsize=(8,4),constrained_layout=True):
+def pdp_interaction(model,df,variables,kind='average',training_only=True,ncols=3,figsize=(8,4),constrained_layout=True):
     """
     Plots interaction partial dependence plots for specified features.
 
     Parameters:
-        automl: AutoML model object.
+        model: AutoML model object.
         df (DataFrame): Input DataFrame containing the dataset.
         variables (list): List of feature names to plot interaction partial dependence plots for.
         kind (str, optional): Type of plot to generate. Default is 'average'.
@@ -110,16 +110,16 @@ def pdp_interaction(automl,df,variables,kind='average',training_only=True,ncols=
     if training_only:
         df = df[df["set"] == "training"]
     fig, ax = plt.subplots(ncols=ncols, figsize=figsize, constrained_layout=constrained_layout)
-    result = PartialDependenceDisplay.from_estimator(automl, df, features=variables,kind=kind,ax=ax)
+    result = PartialDependenceDisplay.from_estimator(model, df, features=variables,kind=kind,ax=ax)
     return result
 
 
-def pdp_nointeraction(automl,df,feature_names,variables=None,kind='average',training_only=True,ncols=3,figsize=(8,4),constrained_layout=True):
+def pdp_nointeraction(model,df,feature_names,variables=None,kind='average',training_only=True,ncols=3,figsize=(8,4),constrained_layout=True):
     """
     Plots partial dependence plots without interaction effects for specified features.
 
     Parameters:
-        automl: AutoML model object.
+        model: AutoML model object.
         df (DataFrame): Input DataFrame containing the dataset.
         feature_names (list): List of feature names to plot partial dependence plots for.
         variables (list, optional): List of variables to plot partial dependence plots for. If None, defaults to feature_names.
@@ -144,7 +144,7 @@ def pdp_nointeraction(automl,df,feature_names,variables=None,kind='average',trai
     interaction_cst = [[name] for name in feature_names]
 
     model_without_interactions = (
-        clone(automl.model.estimator)
+        clone(model.model.estimator)
         .set_params(interaction_constraints = interaction_cst)
         .fit(X_train, y_train))
     fig, ax = plt.subplots(ncols=ncols, figsize=figsize, constrained_layout=constrained_layout)
